fa2d29fd8383ee429b9daf7cbe5695a8aed48efa
Salvak613 <jules.clauwaert@gmail.com>
2025-03-28 10:05:05 +0100

    fixed the bike

--- timetracker.js
+++ timetracker.js
@@ -82,6 +82,9 @@ clickTrainer.addEventListener("click", () => {
 	if (totalClick % 50 === 0) {
 		changeRoad();
 	}
+	if (totalClick % 2400 === 0 && isBikeOn !== true) {
+		bike.style.display = "block"
+	}
 
 
 });
